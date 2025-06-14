eefa2eb586e6e38c6bf740333da8835826271065
saakanbi <woakanbi@gmail.com>
2025-06-13 21:56:22 -0600

    Create outputs.tf

---
+++ terraform/eks/outputs.tf
@@ -0,0 +1,9 @@
+output "cluster_endpoint" {
+  description = "Endpoint for EKS control plane"
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_name" {
+  description = "Name of the EKS cluster"
+  value       = module.eks.cluster_id
+}
